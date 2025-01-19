cf6115b3b995b7ab66327ad9de3ef9761074d468
muhammadibrahim <mik237@gmail.com>
2025-01-19 18:51:20 +0500

    Book provided using compositionLocal to BookImage.kt

---
+++ composeApp/src/commonMain/kotlin/me/ibrahim/bookpedia/kmp/book/presentation/book_list/components/BookImage.kt
@@ -0,0 +1,64 @@
+package me.ibrahim.bookpedia.kmp.book.presentation.book_list.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import bookpedia_kmp.composeapp.generated.resources.Res
+import bookpedia_kmp.composeapp.generated.resources.compose_multiplatform
+import coil3.compose.rememberAsyncImagePainter
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun BookImage() {
+    val book = LocalBookProvider.current
+    Box(
+        modifier = Modifier
+            .height(100.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        var imageLoadResult by remember {
+            mutableStateOf<Result<Painter>?>(null)
+        }
+
+        val painter = rememberAsyncImagePainter(
+            model = book.imageUrl,
+            onSuccess = {
+                imageLoadResult = if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1) {
+                    Result.success(it.painter)
+                } else {
+                    Result.failure(Exception("Invalid Image Size"))
+                }
+            },
+            onError = {
+                it.result.throwable.printStackTrace()
+                imageLoadResult = Result.failure(it.result.throwable)
+            }
+        )
+        when (val result = imageLoadResult) {
+            null -> CircularProgressIndicator()
+            else ->
+                Image(
+                    painter = if (result.isSuccess) painter else painterResource(Res.drawable.compose_multiplatform),
+                    contentDescription = book.title,
+                    contentScale = if (result.isSuccess) ContentScale.Crop else ContentScale.Fit,
+                    modifier = Modifier.aspectRatio(
+                        ratio = 0.65f,
+                        matchHeightConstraintsFirst = true
+                    )
+                )
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/me/ibrahim/bookpedia/kmp/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/me/ibrahim/bookpedia/kmp/book/presentation/book_list/components/BookListItem.kt
@@ -1,33 +1,22 @@
 package me.ibrahim.bookpedia.kmp.book.presentation.book_list.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import bookpedia_kmp.composeapp.generated.resources.Res
-import bookpedia_kmp.composeapp.generated.resources.compose_multiplatform
-import coil3.compose.rememberAsyncImagePainter
 import me.ibrahim.bookpedia.kmp.book.domain.Book
 import me.ibrahim.bookpedia.kmp.theme.LightBlue
-import org.jetbrains.compose.resources.painterResource
+
+val LocalBookProvider = compositionLocalOf<Book> { error("No Book Provided") }
 
 @Composable
 fun BookListItem(
@@ -35,55 +24,22 @@ fun BookListItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Surface(
-        modifier = modifier.clickable(onClick = onClick),
-        shape = RoundedCornerShape(32.dp),
-        color = LightBlue.copy(alpha = 0.2f)
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-                .height(IntrinsicSize.Min)
+
+    CompositionLocalProvider(LocalBookProvider provides book) {
+        Surface(
+            modifier = modifier.clickable(onClick = onClick),
+            shape = RoundedCornerShape(32.dp),
+            color = LightBlue.copy(alpha = 0.2f)
         ) {
-
-            Box(
+            Row(
                 modifier = Modifier
-                    .height(100.dp),
-                contentAlignment = Alignment.Center
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Min)
             ) {
+                BookImage()
 
-                var imageLoadResult by remember {
-                    mutableStateOf<Result<Painter>?>(null)
-                }
 
-                val painter = rememberAsyncImagePainter(
-                    model = book.imageUrl,
-                    onSuccess = {
-                        imageLoadResult = if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1) {
-                            Result.success(it.painter)
-                        } else {
-                            Result.failure(Exception("Invalid Image Size"))
-                        }
-                    },
-                    onError = {
-                        it.result.throwable.printStackTrace()
-                        imageLoadResult = Result.failure(it.result.throwable)
-                    }
-                )
-                when (val result = imageLoadResult) {
-                    null -> CircularProgressIndicator()
-                    else ->
-                        Image(
-                            painter = if (result.isSuccess) painter else painterResource(Res.drawable.compose_multiplatform),
-                            contentDescription = book.title,
-                            contentScale = if (result.isSuccess) ContentScale.Crop else ContentScale.Fit,
-                            modifier = Modifier.aspectRatio(
-                                ratio = 0.65f,
-                                matchHeightConstraintsFirst = true
-                            )
-                        )
-                }
             }
         }
     }
