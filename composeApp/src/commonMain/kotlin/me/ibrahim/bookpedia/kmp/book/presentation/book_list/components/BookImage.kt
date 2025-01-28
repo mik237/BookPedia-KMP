735a840735491d16f7e123b86087744d005049c8
muhammadibrahim <mik237@gmail.com>
2025-01-28 11:15:02 +0500

    rotationX animation applied to book cover image on book list screen

--- composeApp/src/commonMain/kotlin/me/ibrahim/bookpedia/kmp/book/presentation/book_list/components/BookImage.kt
+++ composeApp/src/commonMain/kotlin/me/ibrahim/bookpedia/kmp/book/presentation/book_list/components/BookImage.kt
@@ -1,11 +1,12 @@
 package me.ibrahim.bookpedia.kmp.book.presentation.book_list.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,11 +14,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import bookpedia_kmp.composeapp.generated.resources.Res
 import bookpedia_kmp.composeapp.generated.resources.book_error_2
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import me.ibrahim.bookpedia.kmp.core.presentation.PulseAnimation
 import org.jetbrains.compose.resources.painterResource
@@ -49,6 +53,13 @@ fun BookImage() {
                 imageLoadResult = Result.failure(it.result.throwable)
             }
         )
+
+        val painterState by painter.state.collectAsStateWithLifecycle()
+        val transition by animateFloatAsState(
+            targetValue = if (painterState is AsyncImagePainter.State.Success) 1f else 0f,
+            animationSpec = tween(800)
+        )
+
         when (val result = imageLoadResult) {
             null -> PulseAnimation(modifier = Modifier.size(60.dp))
             else ->
@@ -60,6 +71,12 @@ fun BookImage() {
                         ratio = 0.65f,
                         matchHeightConstraintsFirst = true
                     )
+                        .graphicsLayer {
+                            rotationX = (1f - transition) * 30f
+                            val scale = 0.8f + (0.2f * transition)
+                            scaleX = scale
+                            scaleY = scale
+                        }
                 )
         }
     }
